9d3feed6a5da9044b1d8942122fea4557ddb3d51
Giulio Chiappini <gc@didgt.info>
2015-08-23 15:24:16 +0200

    First Commit

---
+++ print_template.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import yaml
+from jinja2 import Environment, FileSystemLoader
+with open("switch.yml", 'r') as ymlfile:
+    switch = yaml.load(ymlfile)
+env = Environment(loader=FileSystemLoader('.'), trim_blocks=True,
+                  lstrip_blocks=True)
+template = env.get_template('test-template')
+print template.render(switch)
