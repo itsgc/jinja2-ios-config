ed46cbe486397ec059b0d6f1958cb489f8a34516
G.C <gc@didgt.info>
2015-08-26 21:49:25 +0000

    FUNCTIONS! wooosh

--- netmiko_stuff/netmiko_checkvlans.py
+++ netmiko_stuff/netmiko_checkvlans.py
@@ -10,23 +10,25 @@ with open("device_settings.yml", 'r') as ymlfile:
     network_device = yaml.load(ymlfile)
 
 net_connect = ConnectHandler(**network_device)
+vlan_output = net_connect.send_command('show vlan brief')
 
-output = net_connect.send_command('show vlan brief')
-output = output.splitlines()
-output = output[3:]
-active_vlans = dict()
-for line in output:
-	if re.match(r'\S', line):
-		vlan = line.split()
-		vlan_id = int(vlan[0])
-		vlan_name = str(vlan[1])
+def extract_vlans(input):
+	input = input.splitlines()
+	input = input[3:]
+	active_vlans = dict()
+	for line in input:
+		if re.match(r'\S', line):
+			vlan = line.split()
+			vlan_id = int(vlan[0])
+			vlan_name = str(vlan[1])
 		if vlan_id == 1002 or vlan_id == 1003 \
 			or vlan_id == 1004 or vlan_id == 1005:
 			pass
 		else:
 			active_vlans.update({vlan_id:vlan_name})
-
+	return active_vlans
+output = extract_vlans(vlan_output)
 print 'Active VLANs'
 print '{0:10} {1:20}'.format("VLAN ID", "VLAN NAME")
-for keys, values in sorted(active_vlans.iteritems(), key=operator.itemgetter(0)):
+for keys, values in sorted(output.iteritems(), key=operator.itemgetter(0)):
 	print '{0:10} {1:20}'.format(keys,values)
