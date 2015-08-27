601f441edb7d0cd1e846ba0ee445cb0e684044e8
G.C <gc@didgt.info>
2015-08-27 11:35:16 +0000

    Split target definition and common settings. Script can now check vlans for multiple switches

--- netmiko_stuff/netmiko_checkvlans.py
+++ netmiko_stuff/netmiko_checkvlans.py
@@ -8,8 +8,8 @@ from netmiko import ConnectHandler
 
 def netmiko_readconfig(ymlfile):
 	with open(ymlfile, 'r') as ymlfile:
-		network_device = yaml.load(ymlfile)
-		return network_device
+		network_settings = yaml.load(ymlfile)
+		return network_settings
 
 def extract_vlans(input,exceptions=(1002,1003,1004,1005)):
 	input = input.splitlines()
@@ -24,15 +24,18 @@ def extract_vlans(input,exceptions=(1002,1003,1004,1005)):
 	return active_vlans
 
 def print_vlans(input):
-	print 'Active VLANs'
+	print 'Active VLANs for {0}'.format(host)
 	print '{0:10} {1:20}'.format("VLAN ID", "VLAN NAME")
 	for keys, values in sorted(input.iteritems(), key=operator.itemgetter(0)):
 		print '{0:10} {1:20}'.format(keys,values)
 
 active_vlans = dict()
-network_device = netmiko_readconfig("device_settings.yml")
-netmiko_connect = ConnectHandler(**network_device)
-netmiko_output = netmiko_connect.send_command('show vlan brief')
-
-extract_vlans(netmiko_output)
-print_vlans(active_vlans)
+network_settings = netmiko_readconfig("common_settings.yml")
+network_hosts = netmiko_readconfig("hosts.yml")
+for host in network_hosts:
+	network_settings['ip'] = host
+	netmiko_connect = ConnectHandler(**network_settings)
+	netmiko_output = netmiko_connect.send_command('show vlan brief')
+	extract_vlans(netmiko_output)
+	print_vlans(active_vlans)
+	active_vlans = dict()
