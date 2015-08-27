6ec70673c9ac4bfaeb260cff83859122fdcd6659
G.C <gc@didgt.info>
2015-08-27 08:18:10 +0000

    Even More Functions

--- netmiko_stuff/netmiko_checkvlans.py
+++ netmiko_stuff/netmiko_checkvlans.py
@@ -6,12 +6,10 @@ import re
 import operator
 from netmiko import ConnectHandler
 
-with open("device_settings.yml", 'r') as ymlfile:
-    network_device = yaml.load(ymlfile)
-
-net_connect = ConnectHandler(**network_device)
-netmiko_output = net_connect.send_command('show vlan brief')
-active_vlans = dict()
+def netmiko_readconfig(ymlfile):
+	with open(ymlfile, 'r') as ymlfile:
+		network_device = yaml.load(ymlfile)
+		return network_device
 
 def extract_vlans(input,exceptions=(1002,1003,1004,1005)):
 	input = input.splitlines()
@@ -30,6 +28,11 @@ def print_vlans(input):
 	print '{0:10} {1:20}'.format("VLAN ID", "VLAN NAME")
 	for keys, values in sorted(input.iteritems(), key=operator.itemgetter(0)):
 		print '{0:10} {1:20}'.format(keys,values)
+
+active_vlans = dict()
+network_device = netmiko_readconfig("device_settings.yml")
+netmiko_connect = ConnectHandler(**network_device)
+netmiko_output = netmiko_connect.send_command('show vlan brief')
 
 extract_vlans(netmiko_output)
 print_vlans(active_vlans)
